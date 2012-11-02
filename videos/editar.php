88f1d7083663dd05893c552017c5ea155456f6f3
Ulises Rodriguez <ing.ulisesrodriguez@gmail.com>
2012-11-01 21:58:32 -0600

    Video

--- videos/editar.php
+++ videos/editar.php
@@ -208,8 +208,9 @@
   <!-- end scripts -->
 
   <!-- Asynchronous Google Analytics snippet. Change UA-XXXXX-X to be your site's ID.
-       mathiasbynens.be/notes/async-analytics-snippet -->
+       mathiasbynens.be/notes/async-analytics-snippet --> 
   <script>
+  	
     var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
     (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
     g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
